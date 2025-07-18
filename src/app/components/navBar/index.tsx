5df71c843a8ab7fb79aa3cee2210742dd6221319
Tom <t1123425@gmail.com>
2025-07-19 01:18:35 +0800

    fix eslint issue

--- src/app/components/navBar/index.tsx
+++ src/app/components/navBar/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter, usePathname,useSearchParams } from 'next/navigation';
 const navList = [
     {
